17471e5e1aba3686a9103dbf5c60f50d3b2123b3
lujaneshabbir <60037203+lujaneshabbir@users.noreply.github.com>
2022-04-07 22:40:43 +1200

    filter out duplicates feature added

--- reddit.py
+++ reddit.py
@@ -16,31 +16,26 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="fe523ddd3e48476590d0c8
                                                redirect_uri="http://localhost:8888/callback",
                                                scope="playlist-read-private playlist-modify-private"))
 
-# FUNCTIONSfuyhjjhkkj
+# FUNCTIONS
 def add_songs(pl_id, songs):
     id_list = []
     for index in range(len(songs)):
-        print(songs[index])
-        print(index)
         if "https" not in songs[index]:
             if "-" in songs[index]:
                 split = songs[index].split(' -')
             elif "by" in songs[index]:
                 split = songs[index].split(' by')
-            print("split", split)
-        print(len(split))
         search_term = split[0] + split[1]
-        print(split[0])
-        print(split[1])
         # if artist name 'the'
         # if song already in list
         result = sp.search(search_term, limit=1, offset=0, type='track', market=None)
         if result['tracks']['total'] > 0:
             id_list.append(result['tracks']['items'][0]['id'])
+    id_list = set(id_list)
+    id_list = list(id_list)
     sp.playlist_add_items(pl_id, id_list, position=None)
 
 def make_playlist(submission, songs):
-    print("!!!")
     if songs:
         pls = sp.current_user_playlists(limit=50, offset=0)
         pl_found = False
@@ -61,7 +56,7 @@ def find_songs (submission):
     print(songs)
     make_playlist(submission, songs)
 
-post = reddit.submission(id='twfxhm')
+post = reddit.submission(id='tv90ah')
 print(post.title)
 # for comment in post.comments[:1000]:
     # print(comment.body, '\n')
